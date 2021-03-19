60c68f79e362c3739fdc16e6c34276a8e360576a
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2021-03-19 16:42:08 +0700

    sudah jadi, tinggal di improv lagi

--- src/autoRoute.php
+++ src/autoRoute.php
@@ -8,23 +8,93 @@ use Illuminate\Support\Facades\Route;
 
 class autoRoute extends BaseController {
 
+    // default namespace
+    protected static $namespace = 'App\Http\Controllers';
+
+    // default method 
+    protected static $defaultMethod = 'index';
+
     /**
      * To turn on auto route
      */
-    static public function init()
+    static public function init(array $config = [])
     {
-        return Route::any('/{path}', function($path) {
-            $classname = null;
-            $folderName = null;
+        if (
+            isset($config['namespace']) &&
+            !empty($config['namespace'])
+        ) {
+            self::$namespace = $config['namespace'];
+        }
 
+        if (
+            isset($config['defaultMethod']) &&
+            !empty($config['defaultMethod'])
+        ) {
+            self::$defaultMethod = $config['defaultMethod'];
+        }
+
+        return Route::any('/{path}/{path2?}/{path3?}/{path4?}/{path5?}/{path6?}/{path7?}/{path8?}/{path9?}/{path10?}/{path11?}', function(
+            $path, 
+            $path2 = '\\', 
+            $path3 = '\\',
+            $path4 = '\\',
+            $path5 = '\\',
+            $path6 = '\\',
+            $path7 = '\\',
+            $path8 = '\\',
+            $path9 = '\\',
+            $path10 = '\\',
+            $path11 = '\\'
+        ) {
+            // clean path
             $path = strtok($path, '?');
-            $explode = explode('/', $path);
+            $path2 = $path2 == '\\' ? "\\" : "\\" . strtok($path2, '?');
+            $path3 = $path3 == '\\' ? "\\" : "\\" . strtok($path3, '?');
+            $path4 = $path4 == '\\' ? "\\" : "\\" . strtok($path4, '?');
+            $path5 = $path5 == '\\' ? "\\" : "\\" . strtok($path5, '?');
+            $path6 = $path6 == '\\' ? "\\" : "\\" . strtok($path6, '?');
+            $path7 = $path7 == '\\' ? "\\" : "\\" . strtok($path7, '?');
+            $path8 = $path8 == '\\' ? "\\" : "\\" . strtok($path8, '?');
+            $path9 = $path9 == '\\' ? "\\" : "\\" . strtok($path9, '?');
+            $path10 = $path10 == '\\' ? "\\" : "\\" . strtok($path10, '?');
+            $path11 = $path11 == '\\' ? "\\" : "\\" . strtok($path11, '?');
 
-            $classname = $explode[ count($explode) - 1 ];
-            $folder = str_replace($classname, "", $path);
+            // arrange all path
+            $path = $path . $path2 . $path3 . $path4 . $path5 . $path6 . $path7 . $path8 . $path9 . $path10 . $path11;
 
-            echo $class = "\App\Http\Controller\\" . $folder;
+            // remove \\ from last path
+            $path = rtrim($path, "\\");
+            
+            // get class and method name
+            $explode = explode("\\", $path);
+            if (count($explode) === 1) {
+                $methodName = self::$defaultMethod;
+            } else {
+                $methodName = $explode[ count($explode) - 1 ];
+            }
+            $path = str_replace("\\" . $methodName, "", $path);
+
+            // arrange class name
+            $arrangeClassName = self::defaultNamespace() . "\\" . $path;
+            $newClass = new $arrangeClassName;
+
+            // call the method now
+
+            // if class does not exist
+            if (!method_exists($newClass, $methodName)) {
+                return abort(404);
+            }
+
+            return $newClass->$methodName();
         });
+    }
+
+    /**
+     * Get default Controllers namespace
+     */
+    static private function defaultNamespace()
+    {
+        return "\\" . self::$namespace;
     }
 
 }

d955d16f1e332c60da3c59b6af1f8ae6ca06ed17
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2021-03-22 17:11:12 +0700

    add allowed HTTP Method feature

--- config/autoroute.php
+++ config/autoroute.php
@@ -6,4 +6,10 @@ return [
 
     // Default Method
     'defaultMethod' => 'index',
+
+    // Allowed Http
+    // For example if you just only define GET in
+    // allowedHttp array, so you can visit your
+    // auto routing url in GET Method only
+    'allowedHttp' => ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 ];
